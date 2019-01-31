23596efa52184c5b9ca8def76167f1ed4f72f407
Dr.Hacknik <zoltx23@gmail.com>
2019-01-31 18:51:01 -0500

    Starting work on Update Parsing
    * Will Implement Update parsing from AppVeyor, maybe.

--- Source/GetMyBru/GetMyBru/GetMyBru.Services/SvcUpdate.cs
+++ Source/GetMyBru/GetMyBru/GetMyBru.Services/SvcUpdate.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GetMyBru.GetMyBru.Services
 {
-    class SvcUpdate
+    internal class SvcUpdate
     {
+        private static string cd = Environment.CurrentDirectory;
+
+        public void GetUpdate()
+        {
+            using (var JSONData = new WebClient())
+            {
+                JSONData.DownloadFile("https://github.com/DrHacknik/GetMyBru/blob/master/Common/Updates/Meta.json", cd + "\\Data\\Cache\\Update\\Update.json");
+            }
+            ParseUpdate();
+        }
+
+        private void ParseUpdate()
+        {
+            using (StreamReader ReadJson = new StreamReader(cd + "\\Data\\Cache\\Update\\Update.json"))
+            {
+                string json = ReadJson.ReadToEnd();
+                return;
+            }
+        }
     }
 }
