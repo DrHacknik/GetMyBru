044823fffa7a829caeed15b024de1d65bc2aaec2
Dr.Hacknik <zoltx23@gmail.com>
2019-02-25 19:05:20 -0500

    Hardcoding Homebrew list

---
+++ Source/GetMyBru/GetMyBru/GetMyBru.Core/Installer/IHomebrewList.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMyBru.GetMyBru.Core.Installer
+{
+    internal static class IHomebrewList
+    {
+        //Games
+        public static readonly string GameSolarusDX = "solarus";
+
+        public static readonly string GameOpenLara = "Openlara";
+        public static readonly string GameTicTacToe = "TicTacToeNX";
+        public static readonly string Game2048 = "2048";
+        public static readonly string GameLugaru = "Lugaru";
+        public static readonly string GameSpaceGame = "spacenx";
+        public static readonly string GameTiles = "Tiles_NX";
+        public static readonly string GameXash3d = "xash3d-switch_Blue_Shift.zip";
+        public static readonly string GameKGDOOM = "kgdoom";
+        public static readonly string GameLegendOfSwordAndFairy = "pal";
+        public static readonly string GameSuperMarioWar = "supermariowar-nx";
+        public static readonly string GameWhatIsInTheBox = "Whats-in-the-box-NX";
+        public static readonly string GameMeritous = "meritous";
+        public static readonly string GameTilNX = "TIL-NX";
+        public static readonly string GameSnake = "Snake";
+        public static readonly string GameJumpBump = "jumpnbump";
+        public static readonly string GamePixelShuffle = "Pixel_Shuffle_NX";
+        public static readonly string GamesPong = "pong";
+        public static readonly string GameCannonBall = "cannonball";
+        public static readonly string GameTREKT = "T-Rekt-NX";
+        public static readonly string GameGameOfLife = "gol";
+        public static readonly string GameHexGem = "hexgem";
+        public static readonly string GameBermuda = "bs";
+        public static readonly string GameInvaders = "InvadersNX";
+        public static readonly string GamePostal = "Postal";
+        public static readonly string GameTwoPlanes = "2planes";
+        public static readonly string GameMinesweeper = "MinesweeperNX";
+        public static readonly string GameF2BGL = "f2bgl";
+        public static readonly string GameFifteen = "FifteenNX";
+        public static readonly string GameQuakeSpasmNX = "QuakespasmNX";
+        public static readonly string GameWolf4SDL = "Wolf4SDL";
+        public static readonly string GameRawgl = "rawgl";
+        public static readonly string GamePickr = "pickr";
+        public static readonly string GameSDLPong = "sdl_pong";
+        public static readonly string GameEduke32 = "eduke32";
+        public static readonly string GameEnigma = "enigma";
+        public static readonly string GameREMiniscence = "REminiscence";
+        public static readonly string GameDDLCLove = "DDLC";
+        public static readonly string GameHamsterNX = "Hamsters";
+        public static readonly string GameDX2Switch = "dx2-switch";
+        public static readonly string GameSimonNX = "SimonNX";
+        public static readonly string GameHATA = "hata";
+        public static readonly string GameSonicRoboBlastTwo = "sonicroboblast2";
+        public static readonly string GameTetrisSwitch = "tetrisswitch";
+        public static readonly string GameSudokunx = "Sudokunx";
+        public static readonly string GameClawSwitch = "ClawSwitch";
+        public static readonly string GameMazeNX = "MazeNX";
+        public static readonly string GameLIERO = "Liero";
+        public static readonly string GameRocksDiamonds = "rocksanddiamonds";
+        public static readonly string GameChocolateDOOM = "Doom";
+        public static readonly string GamePushANX = "pusha";
+        public static readonly string GameMirrorMagic = "mirrormagic";
+        public static readonly string GameNXEngineEvo = "nxengine";
+        public static readonly string GameMemoryGameNX = "memory-game-nx";
+        public static readonly string GameKensLab = "Kens-Labrynth";
+        public static readonly string GameNXQuake = "nxquake";
+        public static readonly string GameDOOM64EX = "doom64";
+
+        //Emulators
+        public static readonly string EmuDesmume = "desmume";
+
+        public static readonly string EmuRetroarch = "RetroNX";
+        public static readonly string EmuKHEDGB = "khedgb";
+        public static readonly string EmuMelondDS = "melonDS";
+        public static readonly string EmuLaines = "laines";
+        public static readonly string EmuGearboy = "gearboy";
+        public static readonly string EmuPicoDrive = "picodrive";
+        public static readonly string EmuPSNES = "psnes";
+        public static readonly string EmuMGBA = "mgba";
+        public static readonly string EmuScummVM = "scummvm";
+        public static readonly string EmuVBANEXT = "VBA_NEXT";
+        public static readonly string EmuMAMENX = "MAME-NX";
+        public static readonly string EmuVirtualJag = "jaguar";
+        public static readonly string EmuGDKGBA = "gdkgba";
+        public static readonly string EmuVBANEXTRetro = "vba-next";
+        public static readonly string EmuBeetlePSX = "beetlepsx";
+        public static readonly string EmuOpenBor = "openbor";
+        public static readonly string EmuGameBatte = "gamebatte";
+        public static readonly string EmuSNES9x2010NX = "snes9x_2010_nx";
+        public static readonly string EmuNEOPOPSDL = "neopop_sdl";
+        public static readonly string EmuMAMERetro = "mame2003";
+        public static readonly string EmuMAME2000Retro = "mame2000";
+        public static readonly string EmuPFBA = "pFBA";
+        public static readonly string EmuSNES9x2010 = "snes9x_2010";
+        public static readonly string EmuUAE4ALL2 = "uae4all2";
+        public static readonly string EmuFCEUMM = "fceumm";
+        public static readonly string EmuCHIP8NX = "CHIP8";
+        public static readonly string EmuGenesisPlusNX = "genesisnx";
+
+        //Tools
+        public static readonly string ToolKEZPLEZ = "kezplez-nx";
+
+        public static readonly string ToolFireplace = "FireplaceNX";
+        public static readonly string ToolGagOrder = "gagorder";
+        public static readonly string ToolNXSHELL = "NX-shell";
+        public static readonly string ToolVaperSpec = "Vaper_spec";
+        public static readonly string ToolBotwSaveEditor = "botwsaveeditor";
+        public static readonly string ToolSwitchXBOXController = "switchXBOXcontroller";
+        public static readonly string ToolFTPDSXOS = "FTPD4SXOS";
+        public static readonly string ToolNXSH = "nxsh";
+        public static readonly string ToolPixelPainter = "pixel-painter";
+        public static readonly string ToolEasyPower = "easypower";
+        public static readonly string ToolSharedFont = "sharedfontdumper";
+        public static readonly string ToolKEX = "Kex";
+        public static readonly string ToolLockPick = "Lockpick";
+        public static readonly string ToolAvatool = "Avatool";
+        public static readonly string ToolKIPSELECT = "KipSelect";
+        public static readonly string ToolBrewJS = "brewjs";
+        public static readonly string ToolPPlay = "pplay";
+        public static readonly string ToolN1DUS = "n1dus";
+        public static readonly string ToolYALLARENUTS = "yallarenuts";
+        public static readonly string ToolScreenTesterNX = "ScreenTester";
+
+        //Advanced
+        public static readonly string AdvArgonNX = "argon-nx";
+
+        public static readonly string AdvSysNETCHEAT = "sys-netcheat";
+        public static readonly string AdvSLIDENX = "SlideNX";
+        public static readonly string AdvSysFTPD = "sys-ftpd";
+        public static readonly string AdvReiNX = "ReiNX";
+        public static readonly string AdvSysClk = "sys-clk";
+        public static readonly string AdvSwitchPresence = "switchpresence";
+        public static readonly string AdvEbooksCollection = "ebooks";
+        public static readonly string AdvNXTHEMESINSTALLER = "NXThemes_Installer";
+        public static readonly string AdvHekate = "Hekate";
+        public static readonly string AdvXORPLAY = "xor_play";
+    }
+}
