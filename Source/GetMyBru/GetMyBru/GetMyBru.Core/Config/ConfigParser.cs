80da99f7e12d6a21aa37b5a516ca58828b48ab0c
Dr.Hacknik <zoltx23@gmail.com>
2019-03-25 13:34:35 -0400

    Starting to implement settings management
    * Will allow settings to be stored in the executable directory rather than being stored in `%appdata%`

--- Source/GetMyBru/GetMyBru/GetMyBru.Core/Config/ConfigParser.cs
+++ Source/GetMyBru/GetMyBru/GetMyBru.Core/Config/ConfigParser.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; 
 
 namespace GetMyBru.GetMyBru.Core.Config
 {
     internal class ConfigParser
     {
+        public static bool AutoUpdate;
+        public static bool Branch; 
+        public static bool Clean;
+        public static bool Notif; 
+        public static bool FirstTime; 
+        public static bool Drive; 
+        
+        public static void CheckSettings()
+        {
+            if (!File.Exists(Environment.CurrentDirectory + "\\app.config"))
+            {
+                Configuration AppConfig = ConfigurationManager.OpenExeConfiguration(Environment.CurrentDirectory);
+                AppConfig.Save(ConfigurationSaveMode.Minimal);
+            }
+            else
+            {
+                LoadSettings();
+                return;
+            }
+        }
+        
+        public static void SaveSettings()
+        {
+            return;
+        }
+        
+        public static void LoadSettings()
+        {
+            return;
+        }
     }
 }
